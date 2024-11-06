8435b308f7fe00ed98849b0d745a742f6f572dc9
Əyyub Əkbərov <yyubekber4@gmail.com>
2024-11-06 20:11:14 +0400

    final

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var a = 5;
+var b = "salam"
+var c = Date()
+var d = "how are you?"
+var e = "where are you from?"
+var E = "Azerbaijan"
+var n = "English"
+var N = "France"
+var m = "Turkey"
+var M = "India"
+var s = "hello"
+var S = "hi"
+var w = "fine"
+var W = "bad"
+var y = 1
+var Y = y+1
+var t = Y+1
+var T = t+1
+var z = T+1
+var Z = z+1
+var v = e+" "+E
+var V = e+" "+n
+var as = e+" "+N
+var AS = e+" "+m
+var aS = e+" "+M
+document.write(y+"        "+t+"   "+z)
+document.write(as+"        "+V+"   "+v+" "+AS+"        "+aS+"   "+Y)
